456c11647fd417e17f81882d3f3aa8afde4c25e4
Asane <yaradayo317@gmail.com>
2021-02-06 22:51:38 +0900

    updated menuitem to navigate wirhRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import './App.css';
-import HomePgae from "./components/homepage/HomePage";
+import HomaPage from "./components/homepage/HomePage";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePgae />
+      <Switch>
+        <Route exact path="/" component={HomaPage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
