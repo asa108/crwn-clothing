a7ea48f9e1287c6a26ba7cdff660014e407c6237
Asane <yaradayo317@gmail.com>
2021-02-07 09:59:30 +0900

    added Header component and navigated shop and home

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Route, Switch } from "react-router-dom";
 import './App.css';
 import HomaPage from "./page/homepage/HomePage.jsx";
 import ShopPage from "./page/shop/shop.component";
-
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomaPage} />
         <Route path="/shop" component={ShopPage} />
