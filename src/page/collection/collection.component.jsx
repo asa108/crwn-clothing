ff8295a4828e622fc94e12c77b569a122480a759
Asane <yaradayo317@gmail.com>
2021-02-17 17:00:18 +0900

    added category page to show category item and changed naming of category to collection

---
+++ src/page/collection/collection.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+import "./collection.style.scss";
+
+const CollectionPage = ({ match }) => (
+  <div className="colletion-page">
+    <h2>CATEGORY PAGE</h2>
+  </div>
+);
+
+export default CollectionPage;
--- src/page/shop/shop.component.jsx
+++ src/page/shop/shop.component.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/collection-overview/collection-overview";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionOverview />
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
-    
-
 
 export default ShopPage;
