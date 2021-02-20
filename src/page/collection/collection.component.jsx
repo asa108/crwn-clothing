ffcf737c7035d80d850d7008e0551e281c6dda07
Asane <yaradayo317@gmail.com>
2021-02-20 13:16:22 +0900

    show items on category page and styled.

--- src/page/collection/collection.component.jsx
+++ src/page/collection/collection.component.jsx
@@ -6,13 +6,18 @@ import { selectCollection } from "../../redux/shop/shop.selector";
 import "./collection.style.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
-  
+  const { title, items } = collection;
   return (
-  <div className="colletion-page">
-    <h2>CATEGORY PAGE</h2>
-  </div>
-)};
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
