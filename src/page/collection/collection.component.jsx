d174ce563ffa3edf092826fc7bc867fe765e76b1
Asane <yaradayo317@gmail.com>
2021-02-20 13:03:51 +0900

    modified route which take us to gategory page

--- src/page/collection/collection.component.jsx
+++ src/page/collection/collection.component.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
+import { selectCollection } from "../../redux/shop/shop.selector";
 import "./collection.style.scss";
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
+  
+  return (
   <div className="colletion-page">
     <h2>CATEGORY PAGE</h2>
   </div>
-);
+)};
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
