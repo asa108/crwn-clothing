6a7c75911d5e1538736d24608db17a5bee25cf79
Asane <yaradayo317@gmail.com>
2021-02-20 14:10:14 +0900

    comment out image url

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -17,7 +17,7 @@ const StripeCheckoutButton = ({ price }) => {
       name="CROWN Clithing Ltd."
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/en/f3eb2117da"
+      //   image="https://sendeyo.com/up/d/f3eb2117da"
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Buy it now"
